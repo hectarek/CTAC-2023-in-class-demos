213b921842a3f8558a6daa5a74ca617222f2a36b
hectarek <hectarek13@gmail.com>
2023-04-03 13:15:15 -0400

    adding day21

---
+++ day21-intro-to-js/script.js
@@ -0,0 +1,72 @@
+// console.log("Hey Class");
+
+// let dog = 'Tofu';
+// const bird = 'sir chirp';
+
+// let myNumber = 233535324;
+
+// let myBoolean = true;
+// let myOtherBoolean = false;
+
+// let myLargeNumber = 23425892342394320948828937498273947234;
+
+// let num1 = null;
+// let num2;
+// let num3 = undefined;
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+// let myVar = "32452";
+// let mySecondVar = 32452;
+
+// let sdlkfj = 'this';
+
+// let g = 34;
+
+// g = 'giraffe';
+
+// typeof g;
+
+// console.log(typeof g);
+
+let username = '';
+
+// let thisData = 'This'
+
+// console.log(thisData);
+
+// thisData = 'SomethingElse';
+
+// console.log(thisData)
+
+// Implicit Coercion
+
+// let sum = 4+4+4;
+
+// console.log(typeof sum);
+
+// let sum1 = 4+4+"4";
+
+// let mySentence = "This " + "is " + "a " + "sentence";
+
+// console.log(typeof sum1);
+
+// console.log(mySentence);
+
+// let myNums = 2345;
+
+// console.log(typeof myNums);
+
+// let numNumString = myNums.toString();
+
+// console.log(typeof numNumString)
+
+let myNum = 36;
+
+let myOther = "36"
+
+if (myNum > 20 && myNum < 35) {
+    console.log("they are the same");
+} 
