5fd2939be023c884271117f4051ae80259213b6a
hectarek <hectarek13@gmail.com>
2023-06-16 10:16:17 -0400

    add more stuff

--- day74-database-demo/src/main/java/org/example/Main.java
+++ day74-database-demo/src/main/java/org/example/Main.java
@@ -15,13 +15,15 @@ public class Main {
         // Need to create a connection to the DB
         Connection connection = null;
         try {
-            // The properties taht go into your connection
-            String url = "jdbc:mysql://localhost:3306/demo";
-            String username = "root";
-            String password = "mydata1313";
+            // The properties that go into your connection
+            String url = "jdbc:mysql://localhost:3306/yourDB";
+            String username = "YourUsername";
+            String password = "YourPassword";
 
             connection = DriverManager.getConnection(url, username, password);
+
             System.out.println("Successfully connected to the database!");
+
         } catch (SQLException e) {
             System.out.println("Error connecting to the database.");
             e.printStackTrace();
@@ -68,6 +70,77 @@ public class Main {
                 e.printStackTrace();
             }
         }
+
+    }
+
+    public void insertData() {
+
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        try {
+            // The actual connection that was established
+            connection = this.connect();
+
+            pstmt = connection.prepareStatement("INSERT INTO Products (ProductID, ProductName, Category, Price, Stock) VALUES (?, ?, ?, ?, ?)");
+
+            pstmt.setInt(1, 11);
+            pstmt.setString(2, "Lamp Model E");
+            pstmt.setString(3, "Furniture");
+            pstmt.setDouble(4, 50.00);
+            pstmt.setInt(5, 25);
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Error executing SELECT statement");
+            e.printStackTrace();
+        } finally {
+            try {
+                // Need to close each respective parts for security
+                if (pstmt != null) pstmt.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    public void updateProducts() throws SQLException {
+
+        Connection connection = null;
+        Statement stmt = null;
+
+        try {
+            // The actual connection that was established
+            connection = this.connect();
+
+            // The Actual SQL Statement
+            String update1 = "UPDATE Products SET Stock = Stock - 1 WHERE ProductID = 1";
+            String update2 = "UPDATE Employees SET Salary = Salary + 1000 WHERE EmployeeID = 1";
+
+            stmt = connection.createStatement();
+
+            stmt.executeUpdate(update1);
+            stmt.executeUpdate(update2);
+
+            connection.commit();
+
+        } catch (SQLException e) {
+            connection.rollback();
+            System.out.println("Error executing SELECT statement");
+            e.printStackTrace();
+
+        } finally {
+            try {
+                if (stmt != null) stmt.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
 }
